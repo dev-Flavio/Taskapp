b71094866cf354fac1d730997ebd7ff832060d37
Flavio Holanda <flavio.iholanda@hotmail.com>
2024-05-09 21:06:14 -0300

    Botao

---
+++ src/components/button.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "utils/theme";
+
+const Button = () => {
+  return (
+    <Box bg="primary" p="4" borderRadius="rounded-3x1">
+      <Text color="white">Button component on updates</Text>
+    </Box>
+  );
+};
+
+export default Button;
---
+++ src/utils/theme/text-variants.ts
@@ -0,0 +1,38 @@
+export const textVariants = {
+  defaults: {},
+  textXs: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: "gray8",
+  },
+  textSm: {
+    fontSize: 14,
+    lineHeight: 18,
+    color: "gray8",
+  },
+  textBase: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "gray8",
+  },
+  textLg: {
+    fontSize: 18,
+    lineHeight: 28,
+    color: "gray8",
+  },
+  textX1: {
+    fontSize: 20,
+    lineHeight: 28,
+    color: "gray8",
+  },
+  text2X1: {
+    fontSize: 24,
+    lineHeight: 32,
+    color: "gray8",
+  },
+  text3x1: {
+    fontSize: 36,
+    lineHeight: 40,
+    color: "gray8",
+  },
+};
