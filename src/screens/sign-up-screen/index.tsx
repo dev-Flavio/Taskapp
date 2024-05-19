46eb28793bd447d0c6846c2e3bd5d91a700123e3
Flavio Holanda <flavio.iholanda@hotmail.com>
2024-05-18 22:34:59 -0300

    axios

--- src/screens/sign-up-screen/index.tsx
+++ src/screens/sign-up-screen/index.tsx
@@ -4,11 +4,28 @@ import { Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { AuthScreenNavigationType } from "@/navigation/types";
 import SafeAreaWrapper from "@/components/shared/safe-area-wrapper";
+import { registerUser } from "@/service/api";
+import { IUser } from "@/types";
 
 const SignUpScreen = () => {
   const navigation = useNavigation<AuthScreenNavigationType<"SignUp">>();
   const navigateToSignInScreen = () => {
     navigation.navigate("SignIn");
+  };
+
+  const onSubmit = async (data: IUser) => {
+    try {
+      const { email, name, password } = data;
+      /**
+       * register user
+       */
+      await registerUser({
+        email,
+        name,
+        password,
+      });
+      navigateToSignInScreen();
+    } catch (error) {}
   };
 
   return (
